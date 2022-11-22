d480c4a1968ec8e6f61f7c5d4866c1fa67a54dc8
JoseManuelRodriguezCortes <92427972+yonlee03@users.noreply.github.com>
2022-11-22 13:45:19 +0100

    Add files via upload

---
+++ src/Pagina_Home/homeMain.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pagina_Home;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+/**
+ *
+ * @author JoseManuelRodriguezC
+ */
+public class homeMain extends JFrame {
+
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
