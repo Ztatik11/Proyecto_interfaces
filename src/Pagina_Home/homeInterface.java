baf697b31283335247bfb355243f692cb52c71b7
Ztatik11 <99404495+Ztatik11@users.noreply.github.com>
2022-12-01 13:52:57 +0100

    Add files via upload

--- src/Pagina_Home/homeInterface.java
+++ src/Pagina_Home/homeInterface.java
@@ -8,14 +8,12 @@ package Pagina_Home;
  *
  * @author JoseManuelRodriguezC
  */
-import java.awt.Component;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JLabel;
 public class homeInterface extends javax.swing.JFrame {
 
     /**
@@ -475,17 +473,7 @@ public class homeInterface extends javax.swing.JFrame {
     }//GEN-LAST:event_menuButtonActionPerformed
 
     private void searchButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_searchButtonMouseClicked
-        
-        JLabel[] labelJuego = {
-        G1,
-        G2,
-        G3,
-        G4,
-        G5,
-        G6
-        };
-        try {
-            
+      try {
            ArrayList<String> juegos = new ArrayList<String>();
             java.sql.ResultSet resultSet=conexion_db.realizar_consulta("SELECT * FROM juegos");
             
@@ -496,20 +484,9 @@ public class homeInterface extends javax.swing.JFrame {
             }
             for(int i = 0; juegos.size()>i;i++){
                 if(busqueda.equalsIgnoreCase(juegos.get(i))){
-                    for(int j=0;labelJuego.length>j;j++ ){
-                        if(juegos.get(i).equalsIgnoreCase(labelJuego[j].getText())==false){
-                            JLabel juegoE=labelJuego[j];
-                            juegoE.getParent().setVisible(false);     
-                        }
-                    }
-                }if(busqueda.equalsIgnoreCase("")){
-                for(int j=0;labelJuego.length>j;j++ ){      
-                      JLabel juegoE=labelJuego[j];
-                      juegoE.getParent().setVisible(true);         
-                    }
+                    System.out.println(juegos.get(i));
                 }
             }
-            
         } catch (SQLException ex) {
             Logger.getLogger(homeInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
