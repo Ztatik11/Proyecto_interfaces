568e17d670b615a64b01788e2a0ced73182ee945
Sonicorndb <99404484+Sonicorndb@users.noreply.github.com>
2022-12-14 19:51:40 +0100

    Jlabels arreglaos

--- src/Pagina_Home/InterfazJuego.java
+++ src/Pagina_Home/InterfazJuego.java
@@ -40,10 +40,10 @@ public class InterfazJuego extends javax.swing.JFrame {
         this.ImagenJuego.setIcon((new ImageIcon(convertir_imagen())));
         this.LabelTitulo.setText(this.juego.getTitulo());
         this.LabelDescripcion.setText(this.juego.getDescripcion());
-        this.LabelDesarrolladora.setText(this.juego.getDesarrolladora());
-        this.LabelPrecio.setText(String.valueOf(this.juego.getPrecio()) + "€");
-        this.LabelNota.setText(this.juego.getNota());
-        this.LabelNjugadores.setText(this.juego.getNumero_jugadores());
+        this.LabelDesarrolladora.setText("Desarrolladora: " +this.juego.getDesarrolladora());
+        this.LabelPrecio.setText("Precio: " + String.valueOf(this.juego.getPrecio()) + "€");
+        this.LabelNota.setText("Nota: " +this.juego.getNota() + "/10");
+        this.LabelNjugadores.setText(this.juego.getNumero_jugadores() + " jugadores");
         this.LabelGenero.setText(this.juego.getGenero());
     }
     
@@ -78,7 +78,6 @@ public class InterfazJuego extends javax.swing.JFrame {
         Boton_login = new javax.swing.JButton();
         Boton_registro = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
-        menuButton = new javax.swing.JButton();
         informacion_juego = new javax.swing.JPanel();
         Imagenes_precio = new javax.swing.JPanel();
         Boton_comprar = new javax.swing.JButton();
@@ -86,8 +85,8 @@ public class InterfazJuego extends javax.swing.JFrame {
         LabelNota = new javax.swing.JLabel();
         ImagenJuego = new javax.swing.JButton();
         Informacion_juego = new javax.swing.JPanel();
-        LabelDescripcion = new javax.swing.JLabel();
         LabelTitulo = new javax.swing.JLabel();
+        LabelDescripcion = new javax.swing.JLabel();
         LabelGenero = new javax.swing.JLabel();
         LabelDesarrolladora = new javax.swing.JLabel();
         LabelNjugadores = new javax.swing.JLabel();
@@ -258,17 +257,12 @@ public class InterfazJuego extends javax.swing.JFrame {
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/MicrosoftTeams-image (2).png"))); // NOI18N
         jLabel5.setPreferredSize(new java.awt.Dimension(100, 100));
 
-        menuButton.setBackground(new java.awt.Color(0, 153, 255));
-        menuButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/homeIcon.png"))); // NOI18N
-
         javax.swing.GroupLayout BannerLayout = new javax.swing.GroupLayout(Banner);
         Banner.setLayout(BannerLayout);
         BannerLayout.setHorizontalGroup(
             BannerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(BannerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(menuButton)
-                .addGap(65, 65, 65)
+                .addGap(142, 142, 142)
                 .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(40, 40, 40)
                 .addComponent(jLabel4)
@@ -293,10 +287,7 @@ public class InterfazJuego extends javax.swing.JFrame {
                         .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BannerLayout.createSequentialGroup()
                         .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(15, 15, 15))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BannerLayout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(menuButton))))
+                        .addGap(15, 15, 15))))
         );
 
         Ventana_juego.add(Banner, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 880, 140));
@@ -341,8 +332,8 @@ public class InterfazJuego extends javax.swing.JFrame {
                             .addComponent(ImagenJuego, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(Imagenes_precioLayout.createSequentialGroup()
                                 .addComponent(LabelPrecio)
-                                .addGap(85, 85, 85)
-                                .addComponent(LabelNota, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addGap(175, 175, 175)
+                                .addComponent(LabelNota, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(Imagenes_precioLayout.createSequentialGroup()
                         .addGap(155, 155, 155)
                         .addComponent(Boton_comprar)))
@@ -371,20 +362,20 @@ public class InterfazJuego extends javax.swing.JFrame {
         Informacion_juego.setBackground(new java.awt.Color(102, 204, 255));
         Informacion_juego.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        LabelDescripcion.setText("Descripcion");
-        Informacion_juego.add(LabelDescripcion, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 78, -1, -1));
-
         LabelTitulo.setText("Titulo");
         Informacion_juego.add(LabelTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 44, -1, -1));
 
+        LabelDescripcion.setText("<html>Descripcion<html>");
+        Informacion_juego.add(LabelDescripcion, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 78, -1, -1));
+
         LabelGenero.setText("Genero");
-        Informacion_juego.add(LabelGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 267, -1, -1));
+        Informacion_juego.add(LabelGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 113, 80, 20));
 
         LabelDesarrolladora.setText("Desarrolladora");
-        Informacion_juego.add(LabelDesarrolladora, new org.netbeans.lib.awtextra.AbsoluteConstraints(54, 165, -1, -1));
+        Informacion_juego.add(LabelDesarrolladora, new org.netbeans.lib.awtextra.AbsoluteConstraints(54, 151, 80, 20));
 
         LabelNjugadores.setText("Nº de jugadores");
-        Informacion_juego.add(LabelNjugadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 199, -1, -1));
+        Informacion_juego.add(LabelNjugadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(55, 185, -1, 30));
 
         informacion_juego.add(Informacion_juego, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 0, 420, 438));
 
@@ -564,7 +555,6 @@ public class InterfazJuego extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JButton menuButton;
     public javax.swing.JPanel panel_excepcion_compra;
     // End of variables declaration//GEN-END:variables
 }
